989a39d92dcd397bdfe393510620b37b7ac07b12
Helen <shlenak@inbox.ru>
2023-08-01 20:02:48 +0200

    Домашка 2 4 6 8 10 13 15

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача №5 
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// Например:
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("input your num: ");
+string? inputNum = Console.ReadLine();
+if (inputNum != null)
+{
+    int num = int.Parse(inputNum);
+    for (int i = 0; i < num * 2; i++)
+    {
+        Console.Write(i - num);
+        Console.Write(", ");
+    }
+    Console.Write(num);
+}
+
+// //Считываем данные с консоли
+// string? imputNumLine = Console.ReadLine();
+// // Проверяем, чтобы данные не были пустыми
+// if (imputNumLine != null)
+// {
+//     // Парсим введенное число
+//     int imputNumberA = int.Parse(imputNumLine);
+//     // Вводим промежуточное значение
+//     int imputNumberC = imputNumberA * (-1);
+//     // Выходные значения
+//     string outNumLine = string.Empty;
+//     //
+//     while (imputNumberC < imputNumberA)
+//     {
+//         outNumLine = outNumLine + imputNumberC + ",";
+//         imputNumberC = imputNumberC + 1;
+//     }
+//     outNumLine = outNumLine + imputNumberA;
+//     Console.WriteLine(outNumLine);
+// }
---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Задача №7 
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// Например:
+// 456 -> 6 
+// 782 -> 2 
+// 918 -> 8
+
+//Считываем данные с консоли 
+string? imputNumLine = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if (imputNumLine != null)
+{
+    // Парсим введенное число
+    int imputNumberA = int.Parse(imputNumLine);
+
+    int lastDigit = imputNumberA % 10;
+
+    Console.WriteLine(lastDigit);
+
+}
